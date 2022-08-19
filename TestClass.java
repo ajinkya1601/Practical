5a9921ef68638f0deaadb307f34c0ef8b1f470e7
Ajinkya Hatwar <ajinkya.hatwar@gmail.com>
2022-08-19 17:02:19 +0530

    print statement added with wrong syntax test 2

--- TestClass.java
+++ TestClass.java
@@ -2,6 +2,6 @@ public class TestClass {
 
 	public static void main(String[] args) {
 		System.out.println("Hello");
-		System.out.println("Hello with wrong syntax")
+		System.out.println(Hello with wrong syntax);
 	}
 }
